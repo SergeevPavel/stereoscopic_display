0393df4da859b10cceefef5404f5923cdff8aa79
createNgo <igrocki@mail.ru>
2014-11-07 11:59:15 +0400

    Add maze

---
+++ DemoMaze/Assets/Scripts/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+using System;
+
+public class CameraController : MonoBehaviour {
+
+	[DllImport("Controller")]
+	private static extern int initController();
+	[DllImport("Controller")]
+	private static extern int deinitController();
+	[DllImport("Controller")]
+	private static extern float getVerticalAngle();
+	[DllImport("Controller")]
+	private static extern float getHorizontalAngle();
+
+	[DllImport("kernel32", SetLastError=true)]
+	static extern bool FreeLibrary(IntPtr hModule);
+	[DllImport("kernel32", SetLastError=true)]
+	static extern IntPtr GetModuleHandle(string ModuleName);
+
+	public int rotation = 50;
+	public int maxAngle = 50;
+	public float delta = 0.1f;
+	private GameObject leader;
+	private GameObject player;
+	private float currentAngle = 0;
+	private float prevAngle = 0;
+	
+	// Use this for initialization
+	void Start () {
+		initController ();
+		leader = GameObject.FindGameObjectWithTag("Leader");
+		player = GameObject.FindGameObjectWithTag ("Player");
+		currentAngle = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		prevAngle = currentAngle;
+		currentAngle = getHorizontalAngle () * maxAngle;
+		if (Math.Abs (currentAngle - prevAngle) <= delta)
+						currentAngle = prevAngle;
+		transform.RotateAround(leader.transform.position, leader.transform.up, currentAngle - prevAngle);
+		                              
+		if (Input.GetKey (KeyCode.Space))
+		{
+			transform.position = player.transform.position;
+			transform.position += transform.forward * 0.18f;
+			transform.rotation = player.transform.rotation;
+			transform.Rotate(Vector3.up, 90);
+			currentAngle = 0;
+		}
+	}
+
+	void OnApplicationQuit()
+	{
+		deinitController ();
+		FreeLibrary (GetModuleHandle ("Controller"));
+	}
+}
---
+++ DemoMaze/Assets/Scripts/ObjectController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.InteropServices;
+
+public class ObjectController : MonoBehaviour {
+
+	public bool rendered = true;
+	public int velocity = 1;
+	public int rotation = 45;
+	private moveDirection state;
+	private enum moveDirection{
+		Stop,
+		Forward, 
+		Backward,
+	};
+
+	//use this for initialization
+	void Start () {
+		renderer.enabled = rendered;
+		rigidbody.detectCollisions = true;
+		rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+	}
+
+
+
+	void moveForward ()
+	{
+		transform.position += transform.right * velocity * Time.deltaTime;
+	}
+	
+	void moveBackward ()
+	{
+		transform.position -= transform.right * velocity * Time.deltaTime;
+	}
+	
+	void moveLeft ()
+	{
+		transform.Rotate (Vector3.down * rotation * Time.deltaTime);
+	}
+	
+	void moveRight ()
+	{
+		transform.Rotate (Vector3.up * rotation * Time.deltaTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.S)) {
+			state = moveDirection.Stop;
+		}
+		
+		if (Input.GetKey (KeyCode.W)) {
+			state = moveDirection.Forward;
+			moveForward ();
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			state = moveDirection.Backward;
+			moveBackward();
+		}
+		if (Input.GetKey( KeyCode.A)) {
+			switch( state )
+			{
+			case moveDirection.Stop: 
+				moveLeft();
+				break;
+			case moveDirection.Forward:
+				moveLeft();
+				break;
+			case moveDirection.Backward:
+				moveRight();
+				break;
+			}
+		}
+		if (Input.GetKey( KeyCode.D)) {
+			switch( state )
+			{
+			case moveDirection.Stop: 
+				moveRight();
+				break;
+			case moveDirection.Forward:
+				moveRight();
+				break;
+			case moveDirection.Backward:
+				moveLeft();
+				break;
+			}
+		}
+	}
+}
